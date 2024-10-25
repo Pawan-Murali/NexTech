e795d0046e10e89786b4985cac0ca86c047eb5e8
Pawan Murali <127650296+Pawan-Murali@users.noreply.github.com>
2024-10-26 00:42:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 
 // Dictionary of "honest" quotes
 const honestQuotes = {
-    "self_doubt": [
-        "Why bother? You're not that special.",
+    "insecurity": [
+        "Why bother? You're not special.",
         "Confidence? Not in this lifetime.",
         "Why try when it's easier to fail?"
     ],
     "procrastination": [
-        "You’ll probably do it tomorrow…or next year.",
+        "There's always going to be a tomorrow. If not... then even better.",
         "Deadlines are just suggestions, right?",
         "Maybe if you ignore it, it’ll go away."
     ],
@@ -16,7 +16,40 @@ const honestQuotes = {
         "Abs are overrated. Have a donut.",
         "Lifting the remote counts as exercise.",
         "Why run when you can sit?"
-    ]
+    ],
+    "academics":[
+        "You'll fail even if you study hard. So binge that show!."
+        "You can't make it in a single day.",
+        "You don't have the talent to study",
+        "Anyway you are not going to pass.",
+    ],
+    
+    "athletics":[
+        "Everyone knows you're going to lose.",
+        "This match is going to be impossible for you.",
+        "The other team looks so good."
+        "It seems like the other team had great practice.",
+    ],
+    
+    
+    "marriage":[
+        "You are clearly the problem.",
+        "You can't be a good partner, it's due to your father.",
+        "Why did you even get married in the first place?",
+    ]/*,
+    
+    "insecurity":
+        "Walking infront of a group of girls alone(roshan subash).",
+        "you suffer tage fear.",
+        "the way u look.",
+        "way of walking.",
+    ],
+    "lovefailure":
+        "selfish",
+        "trust",
+        "expectations",
+        "respect",
+    ]*/
 };
 
 // Function to fetch a random quote for the selected category
